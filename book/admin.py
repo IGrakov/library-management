3e8e7ee8592acf2f979c39f5259ed113f840c7de
Ivan Grakov <ivan.grakov@gmail.com>
2024-10-25 11:56:14 +0300

    Add author and language to admin

--- book/admin.py
+++ book/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from book.models import Book
+from book.models import Author, Book
 
+admin.site.register(Author)
 admin.site.register(Book)
---
+++ reference_values/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from reference_values.models import Language
+
+admin.site.register(Language)
