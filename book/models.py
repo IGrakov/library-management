80f08358dea01b06c407dcec76398d7896e15a62
Ivan Grakov <ivan.grakov@gmail.com>
2024-10-25 10:46:59 +0300

    Update book and author models

--- book/models.py
+++ book/models.py
@@ -1,18 +1,30 @@
 from django.db import models
 
 from core.models import TimeStampMixin
+from reference_values.models import Language
+
+
+class Author(TimeStampMixin):
+    """Model for an author"""
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Author'
 
 
 class Book(TimeStampMixin):
     """Model for a book"""
 
     title = models.CharField(max_length=255, null=False, blank=False)
-    author = models.CharField(max_length=255, null=False, blank=False)
+    author = models.ManyToManyField(Author, null=True)
     published_date = models.DateField(null=True)
-    isbn = models.CharField(max_length=255, null=False, blank=False, unique=True)
+    isbn = models.CharField(max_length=17, null=False, blank=False, unique=True)
     pages = models.IntegerField(null=True)
     cover = models.URLField(null=True)
-    language = models.CharField(max_length=255, null=False, blank=False)
+    language = models.ManyToManyField(Language, null=True)
 
     def __str__(self):
         return (
