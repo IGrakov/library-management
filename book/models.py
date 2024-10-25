e08c4187cb448d5ae95889ee6209d4498768e613
Ivan Grakov <ivan.grakov@gmail.com>
2024-10-25 11:15:41 +0300

    Change author model and make migrations

--- book/models.py
+++ book/models.py
@@ -6,10 +6,12 @@ from reference_values.models import Language
 
 class Author(TimeStampMixin):
     """Model for an author"""
-    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    middle_name = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return ",".join([self.last_name, " ".join([self.first_name, self.middle_name])])
 
     class Meta:
         verbose_name = 'Author'
@@ -17,14 +19,13 @@ class Author(TimeStampMixin):
 
 class Book(TimeStampMixin):
     """Model for a book"""
-
     title = models.CharField(max_length=255, null=False, blank=False)
-    author = models.ManyToManyField(Author, null=True)
+    author = models.ManyToManyField(Author)
     published_date = models.DateField(null=True)
     isbn = models.CharField(max_length=17, null=False, blank=False, unique=True)
     pages = models.IntegerField(null=True)
     cover = models.URLField(null=True)
-    language = models.ManyToManyField(Language, null=True)
+    language = models.ManyToManyField(Language)
 
     def __str__(self):
         return (
