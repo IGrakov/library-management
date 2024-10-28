c9a51557d357fee847285cd1929d142f25591868
Ivan Grakov <ivan.grakov@gmail.com>
2024-10-28 13:13:54 +0300

    Update user admin

--- user/admin.py
+++ user/admin.py
@@ -1,17 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
 from user.models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(UserAdmin):
     @admin.display(description='Groups')
     def group(self, user):
         groups = []
         for group in user.groups.all():
             groups.append(group.name)
-        return ' '.join(groups)
+        return ', '.join(groups)
+
+    fieldsets = (
+        (None, {'fields': ('password',)}),
+        (
+            _('Personal info'),
+            {
+                'fields': (
+                    'email',
+                    'last_name',
+                    'first_name',
+                )
+            },
+        ),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                ),
+            },
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
 
     list_display = (
         'email',
