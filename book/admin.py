32782135c52de00ae9e6f4464bd0a55d30f24c1f
Ivan Grakov <ivan.grakov@gmail.com>
2024-11-06 19:14:14 +0300

    Update author admin

--- book/admin.py
+++ book/admin.py
@@ -8,7 +8,6 @@ from book.models import Author, Book
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'middle_name')
     search_fields = ('last_name',)
-    list_filter = ('last_name',)
     ordering = ('last_name',)
 
     list_per_page = 20
