9b629fdc6256f084bdd946d3bd1bbe9d9d17fc9b
Ivan Grakov <ivan.grakov@gmail.com>
2026-04-28 11:35:11 +0300

    Update constants for reference values

--- backend/reference_values/constants.py
+++ backend/reference_values/constants.py
@@ -7,3 +7,29 @@ class HallTypes(models.TextChoices):
     READING_HALL = 'Reading Hall', 'Reading Hall'
     RARITIES = 'Rarities', 'Rarities'
     WAREHOUSE = 'Warehouse', 'Warehouse'
+
+
+class GenreTypes(models.TextChoices):
+    FICTION = 'Fiction', 'Fiction'
+    NON_FICTION = 'Non-Fiction', 'Non-Fiction'
+    FANTASY = 'Fantasy', 'Fantasy'
+    TRAVEL = 'Travel', 'Travel',
+    HISTORY = 'History', 'History',
+    BIOGRAPHY = 'Biography', 'Biography',
+    ROMANCE = 'Romance', 'Romance',
+    MUSIC = 'Music', 'Music',
+    SCIENCE_FICTION = 'Science Fiction', 'Science Fiction',
+    POETRY = 'Poetry', 'Poetry',
+    CHILDREN = 'Children', 'Children',
+    RELIGION = 'Religion', 'Religion',
+    SCIENCE_TECHNOLOGY = 'Science & Technology', 'Science & Technology',
+    SOCIAL_SCIENCE = 'Social Science', 'Social Science',
+    HOW_TO = 'How-To', 'How-To',
+    HUMOR = 'Humor', 'Humor',
+    CRIME = 'Crime', 'Crime',
+    ART = 'Art', 'Art',
+    FOOD_DRINK = 'Food & Drink', 'Food & Drink',
+    ACTION_ADVENTURE = 'Action & Adventure', 'Action & Adventure',
+    MISTERY = 'Mystery', 'Mystery',
+    THRILLER_SUSPENSE = 'Thriller & Suspense', 'Thriller & Suspense',
+    HORROR = 'Horror', 'Horror'
