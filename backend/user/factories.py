88b59ee9ae62c2ee9cdc3931e014ca3249c07252
Ivan Grakov <ivan.grakov@gmail.com>
2026-04-29 17:31:45 +0300

    Update user factory

--- backend/user/factories.py
+++ backend/user/factories.py
@@ -1,5 +1,8 @@
 import factory
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+
+from user import constants
 
 User = get_user_model()
 
@@ -13,3 +16,24 @@ class UserFactory(factory.django.DjangoModelFactory):
     first_name = factory.Sequence(lambda n: 'first_name%s' % n)
     last_name = factory.Sequence(lambda n: 'last_name%s' % n)
     password = factory.django.Password('password123')
+
+    role = constants.Roles.READER
+
+    @factory.post_generation
+    def assign_role(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        role = extracted or self.role
+
+        group = Group.objects.get(name=role)
+        self.groups.add(group)
+
+        if role in {constants.Roles.ADMIN, constants.Roles.LIBRARIAN}:
+            self.is_staff = True
+            self.is_superuser = False
+        else:
+            self.is_staff = False
+            self.is_superuser = False
+
+        self.save()
