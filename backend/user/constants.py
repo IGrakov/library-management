cd265f491021a072229d27c6dd8e55ad99a01a50
Ivan Grakov <ivan.grakov@gmail.com>
2026-04-30 17:29:56 +0300

    Update user constants and factories

--- backend/user/constants.py
+++ backend/user/constants.py
@@ -5,3 +5,9 @@ class Roles(models.TextChoices):
     ADMIN = 'Admin', 'Admin'
     READER = 'Reader', 'Reader'
     LIBRARIAN = 'Librarian', 'Librarian'
+
+INACTIVE_USER_RANK = -1
+SUPER_USER_RANK = 100
+ADMIN_USER_RANK = 20
+LIBRARIAN_USER_RANK = 10
+READER_USER_RANK = 0
--- backend/user/factories.py
+++ backend/user/factories.py
@@ -10,30 +10,25 @@ User = get_user_model()
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
-        django_get_or_create = ('email',)
+        skip_postgeneration_save = True
 
     email = factory.LazyAttribute(lambda o: '%s@test.com' % f'{o.first_name}.{o.last_name}')
     first_name = factory.Sequence(lambda n: 'first_name%s' % n)
     last_name = factory.Sequence(lambda n: 'last_name%s' % n)
-    password = factory.django.Password('password123')
 
-    role = constants.Roles.READER
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        role = kwargs.pop("role", constants.Roles.READER)
+        user = model_class(*args, **kwargs)
+        user.set_password(kwargs.get("password", "password123"))
 
-    @factory.post_generation
-    def assign_role(self, create, extracted, **kwargs):
-        if not create:
-            return
+        user.is_staff = role in {constants.Roles.ADMIN, constants.Roles.LIBRARIAN}
+        user.save()
 
-        role = extracted or self.role
+        user.groups.clear()
 
-        group = Group.objects.get(name=role)
-        self.groups.add(group)
+        if not user.is_superuser:
+            group, _ = Group.objects.get_or_create(name=role)
+            user.groups.add(group)
 
-        if role in {constants.Roles.ADMIN, constants.Roles.LIBRARIAN}:
-            self.is_staff = True
-            self.is_superuser = False
-        else:
-            self.is_staff = False
-            self.is_superuser = False
-
-        self.save()
+        return user
